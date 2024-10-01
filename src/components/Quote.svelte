856cf1818e9fdcb52e276858dfc3638856147da0
Osnott <hi@osnott.xyz>
2024-09-30 23:22:21 -0700

    working code just need quotes

---
+++ src/components/Quote.svelte
@@ -0,0 +1,37 @@
+<script>
+  import kazumaImg from "$lib/assets/kazuma.png"
+  import { kazuma } from "$lib/quotes.ts"
+  let quote = randomQuote();
+
+  function randomQuote() {
+    return kazuma[Math.floor(Math.random()*kazuma.length)];
+  }
+</script>
+
+<div class="text-box">
+  <img alt="kazuma" class="text-box-image" src={kazumaImg}>
+  <span class="text">{quote}</span>
+</div>
+
+<style>
+  .text-box-image {
+    width: 100%;
+    height: 100%;
+  }
+
+  .text-box {
+    position: relative;
+    text-align: left;
+  }
+
+  .text {
+    position: absolute;
+    top: 75.5%;
+    left: 20%;
+    margin-right: 20%;
+    color: white;
+    font-size: 22pt;
+    font-family: TGAABody;
+    src:url(/fonts/TGAA.otf);
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import Quote from "../components/Quote.svelte";
+</script>
+
+<Quote/>
+
+<style>
+  :global(body) {
+    background-color: #1e1e2e;
+  }
+</style>
